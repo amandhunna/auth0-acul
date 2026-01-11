7bd670af4511cba6940e68f4fa37ccaf613e8ecf
amandhunna-aidash <amanjot.singh@aidash.com>
2026-01-11 23:40:50 +0530

    Deploy login UI assets - sync with Auth0

--- src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-import type { UniversalLoginContext } from "./auth0-sdk";
-
-declare global {
-  interface Window {
-    universal_login_context: UniversalLoginContext;
-  }
-}
-
-export {};
--- src/utils/helpers/iconUtils.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from "react";
-
-import { DefaultConnectionIcon } from "@/assets/icons";
-
-export const getIcon = (): ReactNode => {
-  return <DefaultConnectionIcon />;
-};
